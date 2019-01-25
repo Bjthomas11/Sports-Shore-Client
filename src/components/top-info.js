2f4772864bb9e5050a53e7604d66d857f9c84815
Brian Thomas <Brianthomas571@gmail.com>
2019-01-24 23:57:09 -0600

    adding responsive design

--- src/components/top-info.js
+++ src/components/top-info.js
@@ -5,35 +5,33 @@ import { Link } from "react-router-dom";
 export default class TopInfo extends React.Component {
   render() {
     return (
-      <div>
-        <div id="topInfo">
-          <div id="logo">
-            <Link to={"/"}>
-              <div className="frontPageName">Sports Shore</div>
-            </Link>
-          </div>
+      <div id="topInfo">
+        <div id="logo">
+          <Link to={"/"}>
+            <div className="frontPageName">Sports Shore</div>
+          </Link>
+        </div>
 
-          <div id="buttons">
-            <Link to={"/signup"}>
-              <button
-                className="hvr-underline-from-center"
-                id="signupButton"
-                className="loginButton"
-              >
-                Signup
-              </button>
-            </Link>
+        <div id="buttons">
+          <Link to={"/signup"}>
+            <button
+              className="hvr-underline-from-center"
+              id="signupButton"
+              className="loginButton"
+            >
+              Signup
+            </button>
+          </Link>
 
-            <Link to={"/login"}>
-              <button
-                className="hvr-underline-from-center"
-                id="loginButton"
-                className="loginButton"
-              >
-                Login
-              </button>
-            </Link>
-          </div>
+          <Link to={"/login"}>
+            <button
+              className="hvr-underline-from-center"
+              id="loginButton"
+              className="loginButton"
+            >
+              Login
+            </button>
+          </Link>
         </div>
       </div>
     );
